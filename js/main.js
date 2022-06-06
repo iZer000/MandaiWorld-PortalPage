01f442435a3640a9afb228b83bc78a09e7f077ed
Zero <77457719+iZer000@users.noreply.github.com>
2022-06-06 22:56:52 +0200

    Update: Alles is etz responsive!

--- js/main.js
+++ js/main.js
@@ -4,68 +4,18 @@ window.addEventListener("DOMContentLoaded", function() {
 });
 
 
-
-(function(window,document) {
-
-    var prefix = "", _addEventListener, onwheel, support;
-
-    // detect event model
-    if ( window.addEventListener ) {
-        _addEventListener = "addEventListener";
-    } else {
-        _addEventListener = "attachEvent";
-        prefix = "on";
-    }
-
-    // detect available wheel event
-    support = "onwheel" in document.createElement("div") ? "wheel" : // Modern browsers support "wheel"
-              document.onmousewheel !== undefined ? "mousewheel" : // Webkit and IE support at least "mousewheel"
-              "DOMMouseScroll"; // let's assume that remaining browsers are older Firefox
-
-    window.addWheelListener = function( elem, callback, useCapture ) {
-        _addWheelListener( elem, support, callback,{passive:false}, useCapture );
-
-        if( support == "DOMMouseScroll" ) {
-            _addWheelListener( elem, "MozMousePixelScroll", callback, useCapture );
+var scrollToEl;
+$(function() {
+    var scrollVal = $(window).scrollTop();
+    $(window).scroll(function(){
+        var scrollValNow = $(window).scrollTop();
+        if(scrollVal < scrollValNow && scrollToEl != "secondary") {
+            scrollToEl = "secondary";
+            document.getElementById(scrollToEl).scrollIntoView({behavior: "smooth"});
+        } else if(scrollVal > scrollValNow && scrollToEl != "primary") {
+            scrollToEl = "primary";
+            document.getElementById(scrollToEl).scrollIntoView({behavior: "smooth"});
         }
-    };
-
-    function _addWheelListener( elem, eventName, callback, useCapture ) {
-        elem[ _addEventListener ]( prefix + eventName, support == "wheel" ? callback : function( originalEvent ) {
-            !originalEvent && ( originalEvent = window.event );
-
-            var event = {
-                originalEvent: originalEvent,
-                target: originalEvent.target || originalEvent.srcElement,
-                type: "wheel",
-                deltaMode: originalEvent.type == "MozMousePixelScroll" ? 0 : 1,
-                deltaX: 0,
-                deltaZ: 0,
-                preventDefault: function() {
-                    originalEvent.preventDefault ?
-                        originalEvent.preventDefault() :
-                        originalEvent.returnValue = false;
-                }
-            };
-
-            if ( support == "mousewheel" ) {
-                event.deltaY = - 1/40 * originalEvent.wheelDelta;
-                originalEvent.wheelDeltaX && ( event.deltaX = - 1/40 * originalEvent.wheelDeltaX );
-            } else {
-                event.deltaY = originalEvent.detail;
-            }
-            return callback( event );
-        }, useCapture || false );
-    }
-})(window,document);
-
-
-addWheelListener( document, function( e ) { 
-    if (e.deltaY < 0) {
-        document.getElementById("primary").scrollIntoView();
-    } else {
-        document.getElementById("secondary").scrollIntoView();
-    }
-    
-    e.preventDefault(); 
+        scrollVal = scrollValNow;
+    });
 });
