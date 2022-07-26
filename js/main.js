877322c4e1669fdbfe2f91dcbd084d8e58bc7e1c
Zero <77457719+iZer000@users.noreply.github.com>
2022-07-26 13:09:34 +0200

    Entferne Animatescroll

--- js/main.js
+++ js/main.js
@@ -2,20 +2,3 @@ window.addEventListener("DOMContentLoaded", function() {
     document.getElementById("primary").scrollIntoView();
     document.getElementById("copy").innerHTML = "© " + new Date().getFullYear() + " MandaiWorld";
 });
-
-
-var scrollToEl;
-$(function() {
-    var scrollVal = $(window).scrollTop();
-    $(window).scroll(function(){
-        var scrollValNow = $(window).scrollTop();
-        if(scrollVal < scrollValNow && scrollToEl != "secondary") {
-            scrollToEl = "secondary";
-            document.getElementById(scrollToEl).scrollIntoView({behavior: "smooth"});
-        } else if(scrollVal > scrollValNow && scrollToEl != "primary") {
-            scrollToEl = "primary";
-            document.getElementById(scrollToEl).scrollIntoView({behavior: "smooth"});
-        }
-        scrollVal = scrollValNow;
-    });
-});
